2738a808c3f42e564650d7968966fbaba2059707
Munoz Mendez <Fernando.MunozMendez@aecom.com>
2018-06-07 14:54:25 +0100

    Read data and output trip purposes for match up in excel

--- configuration/requirements.R
+++ configuration/requirements.R
@@ -1,14 +1,16 @@
-require(readr)
-require(tictoc)
-require(RJSONIO) #to read json files
+# require(readr)
+# require(tictoc)
+# require(RJSONIO) #to read json files
 require(openxlsx) #To open excel files
 require(stringr) #To strip the direction from the names
-require(fastmatch) # faster match function
-require(tidyr) #Advanced data manipulation
-require(dplyr) #Advanced data manipulation
-require(lubridate) #To extract the month/day etc ...
-require(fasttime)
-require(chron) #is.weekend function
-require(knitr) #kable tables
+# require(fastmatch) # faster match function
+# require(tidyr) #Advanced data manipulation
+# require(dplyr) #Advanced data manipulation
+# require(lubridate) #To extract the month/day etc ...
+# require(fasttime)
+# require(chron) #is.weekend function
+# require(knitr) #kable tables
 require(ggplot2)
-require(ggthemes) #some themes for better looking plots
+# require(ggthemes) #some themes for better looking plots
+require(tidyverse)
+require(data.table)
